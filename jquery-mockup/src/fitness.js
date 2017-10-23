c88cfa9155ba3d502b2fb188f8eccf1ebd5f1c8b
Anthony DiNardi <NinoD1995@gmail.com>
2017-10-23 16:33:28 -0400

    routines now add to completed routine list

--- jquery-mockup/src/fitness.js
+++ jquery-mockup/src/fitness.js
@@ -2,7 +2,8 @@
 exports.__esModule = true;
 var $ = require("jquery");
 var Routine = /** @class */ (function () {
-    function Routine() {
+    function Routine(name) {
+        this.name = name;
     }
     return Routine;
 }());
@@ -16,10 +17,30 @@ var Tracker = /** @class */ (function () {
         ];
     }
     Tracker.prototype.drawRoutines = function () {
+        var index = 0;
         $("#routine-List").html(this.routineList.map(function (x) { return "<button class=\"list-group-item\">" + x.name + "</button>"; }).join(""));
+        $("#routine-List button").each(function (index) {
+            $(this).attr("id", "id" + index);
+            index++;
+        });
+    };
+    Tracker.prototype.drawMyRoutines = function () {
+        $("#my-routines").html(this.myRoutines.map(function (x) { return "<li class=\"list-group-item\">" + x.name + "</li>"; }).join(""));
     };
     return Tracker;
 }());
 exports.Tracker = Tracker;
+//Controller
 var tracker = new Tracker();
 tracker.drawRoutines();
+$('.list-group-item').click(function (e) {
+    e.preventDefault();
+    //const workoutID = e.target.id;
+    var workoutName = e.target.textContent;
+    document.getElementById('default-message').innerHTML = '';
+    //$("#my-routines").append($("<li class=\"list-group-item\">").text(workoutName));
+    var newRoutine = new Routine(workoutName);
+    tracker.myRoutines.push(newRoutine);
+    console.log(JSON.stringify(tracker.routineList));
+    tracker.drawMyRoutines();
+});
--- jquery-mockup/src/fitness.ts
+++ jquery-mockup/src/fitness.ts
@@ -2,6 +2,10 @@ import * as $ from 'jquery';
 
 export class Routine {
     name: string;
+
+    constructor(name:string) {
+        this.name = name;
+    }
 }
 
 export class Tracker {
@@ -14,9 +18,17 @@ export class Tracker {
 
     
     drawRoutines() {
+
+        const index = 0;
+
         $("#routine-List").html(
-            this.routineList.map(x=> `<button class="list-group-item" id="cmd-add">${x.name}</button>`).join("")
+            this.routineList.map(x=> `<button class="list-group-item">${x.name}</button>`).join("")
         );
+
+        // $("#routine-List button").each(function(index) {
+        //     $(this).attr("id", "id" + index);
+        //     index++;
+        // });
     }
 
     drawMyRoutines() {
@@ -32,11 +44,12 @@ const tracker = new Tracker();
 
 tracker.drawRoutines();
 
-$("#cmd-add").click(function(e) {
+$('.list-group-item').click(function(e) {
     e.preventDefault();
-    const workout = document.getElementById("cmd-add").textContent;
-    tracker.myRoutines = [
-        { name: workout}
-    ];
-    tracker.drawMyRoutines();
-})
+    const workoutName = e.target.textContent;
+
+    document.getElementById('default-message').innerHTML = '';
+    $("#my-routines").append($("<li class=\"list-group-item\">").text(workoutName));
+    //const newRoutine = new Routine(workoutName);
+    //tracker.drawMyRoutines();
+});
